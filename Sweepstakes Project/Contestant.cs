87d371f4d9bb18b458b88f5ce12b6fcbc5ecd9a2
Penson, Kelly <pensonk86@gmail.com>
2021-01-18 08:50:17 -0800

    Created Sweepstakes and UserInterface Classes.

---
+++ Sweepstakes Project/Contestant.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Project
+{
+    class Contestant
+    {
+        //member Variables
+
+        public string firstName;
+        public string lastName;
+        public string email;
+        public int registrationNumber;
+
+
+
+
+        //Constructor
+        public string FirstName { get => firstName; set => firstName = value; }
+        public string LastName { get => lastName; set => lastName = value; }
+        public string Email { get => email; set => email = value; }
+        public int RegistrationNumber { get => registrationNumber; set => registrationNumber = value; }
+        
+
+        //Member Variables
+
+        public Contestant ContestantInformation()
+        {
+            
+
+        }
+
+
+
+    }
+}
---
+++ Sweepstakes Project/UserInterface.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes_Project
+{
+    class UserInterface
+    {
+        public string ContestantFirstName()
+        {
+            Console.WriteLine("Enter Your First Name");
+            string firstNameInput = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            return firstNameInput;
+
+        }
+        public string ContestantLastName()
+        {
+
+            Console.WriteLine("Enter Your Last Name?");
+            string lastNameInput = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            return lastNameInput;
+
+        }
+        public string ContestantEmail()
+        {
+            Console.WriteLine("Enter Your Email Address");
+            string emailInput = Console.ReadLine();
+            Console.WriteLine(" ");
+
+            return emailInput;
+
+        }
+        public int ContestantRegistrationNumber()
+        {
+
+
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
